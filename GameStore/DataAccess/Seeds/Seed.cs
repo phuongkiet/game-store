26c22755637ad86888754734bb9b9070d5c12a56
phuongkiet <kietnp15092003@gmail.com>
2024-07-28 20:07:39 +0700

    config BE

---
+++ GameStore/DataAccess/Seeds/Seed.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataAccess.Seeds
+{
+    public class Seed
+    {
+        public static async Task SeedUser(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            if (await userManager.Users.AnyAsync()) return;
+
+            var userData = await File.ReadAllTextAsync("../DataAccess/Seeds/UserSeed.json");
+            var jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var users = JsonSerializer.Deserialize<List<User>>(userData, jsonOptions);
+
+            var roles = new List<Role>
+            {
+                new Role {Name = "Client"},
+                new Role {Name = "Staff"},
+                new Role {Name = "Manager"},
+                new Role {Name = "Admin"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
+            foreach (var user in users)
+            {
+                user.UserName = user.Email;
+                user.EmailConfirmed = true;
+                var result = await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Client");
+            }
+
+            var admin = new User
+            {
+                UserName = "admin@gmail.com",
+                Name = "admin",
+                Email = "admin@gmail.com",
+                Status = 1,
+                EmailConfirmed = true,
+            };
+
+            var resultAdmin = await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] { "Admin" });
+
+            var manager = new User
+            {
+                UserName = "manager@gmail.com",
+                Name = "manager",
+                Email = "manager@gmail.com",
+                Status = 1,
+                EmailConfirmed = true,
+            };
+
+            await userManager.CreateAsync(manager, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(manager, new[] { "Manager" });
+        }
+    }
+}
--- GameStore/Presentation/Program.cs
+++ GameStore/Presentation/Program.cs
@@ -1,11 +1,98 @@
+using DataAccess;
+using DataAccess.Models;
+using DataAccess.Seeds;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
+builder.Services.AddDbContext<GameStoreDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStringDB")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        RequireExpirationTime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration.GetSection("Jwt:Issuer").Value,
+        ValidAudience = builder.Configuration.GetSection("Jwt:Audience").Value,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value))
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GameStoreAPI", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+builder.Services.AddIdentityCore<User>(opt =>
+{
+    opt.Password.RequiredLength = 5;
+
+    opt.User.RequireUniqueEmail = true;
+})
+    .AddRoles<Role>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddEntityFrameworkStores<GameStoreDbContext>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
@@ -17,9 +104,26 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<GameStoreDbContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var roleManager = services.GetRequiredService<RoleManager<Role>>();
+    await context.Database.MigrateAsync();
+    /*    await context.Database.ExecuteSqlRawAsync("DELETE FROM Connections");
+    */
+    await Seed.SeedUser(userManager, roleManager);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error while seeding data");
+}
 app.Run();
