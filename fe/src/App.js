7e1e5db1cbe3090b0d53d480b842364ecf2fbe65
Mirera <phudongsi1@gmail.com>
2024-07-28 20:04:26 +0700

    fe_commit

---
+++ fe/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p className="text-3xl font-bold text-gray-300">Reactjs + Tailwind</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
