7d37e8e5c469af73c2e5c83c856e595bd8528de3
Mirera <phudongsi1@gmail.com>
2024-07-31 12:57:08 +0700

    Merging

--- GameStore/Presentation/Controllers/GenreController.cs
+++ GameStore/Presentation/Controllers/GenreController.cs
@@ -17,7 +17,7 @@ namespace Presentation.Controllers
         }
 
         [HttpGet("Genres")]
-        //[Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")]
         //IActionResult trả về status code
         public async Task<IActionResult> ListGenre()
         {
